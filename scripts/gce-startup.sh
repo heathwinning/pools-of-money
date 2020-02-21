73cf1ae5037e4285e222f04820748bb1aded108f
Heath Winning <heathwinning@gmail.com>
2020-02-21 12:52:30 +1100

    fix ktor setup

--- scripts/gce-startup.sh
+++ scripts/gce-startup.sh
@@ -7,7 +7,7 @@ PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/
 echo "Project ID: ${PROJECTID}"
 
 # Install dependencies from apt
-apt-get install -yq openjdk-8-jdk git
+apt-get install -yq openjdk-11-jdk git
 
 # Jetty Setup
 mkdir -p /opt/jetty/temp
@@ -18,7 +18,7 @@ curl -L https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.4.
 tar xf jetty9.tgz  --strip-components=1 -C /opt/jetty
 
 # Add a Jetty User
-useradd --user-group --shell /bin/false --home-dir /opt/jetty/temp jetty
+useradd --user-group --shell /bin/false --home-dir /opt/jetty/temp jetty || echo "user jetty already exists"
 
 cd /opt/jetty
 # Add running as "jetty"
@@ -26,13 +26,18 @@ java -jar /opt/jetty/start.jar --add-to-startd=setuid
 cd /
 
 # Clone the source repository.
+if [ -d pools-of-money ] ; then
+  echo "directory pools-of-money already exists, removing..."
+  rm -rf pools-of-money
+fi
 git clone https://github.com/heathwinning/pools-of-money.git
 cd pools-of-money
 
 # Build the .war file and rename.
 # very important - by renaming the war to root.war, it will run as the root servlet.
-./gradlew war
-mv build/lib/moneypools.war /opt/jetty/webapps/root.war
+#./gradlew war
+cp /home/heath/moneypools.war /opt/jetty/webapps/root.war
+echo "Created war file and moved to root.war"
 
 # Make sure "jetty" owns everything.
 chown --recursive jetty /opt/jetty
